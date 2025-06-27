8011f621454cc5bbe8687a6fb7fc0ae3d783506c
tassioFront <tassio72@hotmail.com>
2025-06-27 17:50:41 -0300

    chore: create first tests

---
+++ backend/datapulse/test_settings.py
@@ -0,0 +1,48 @@
+"""
+Test settings for Django project
+"""
+from .settings import *
+
+# Use SQLite for testing (faster and no external dependencies)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+# Remove WhiteNoise middleware for tests
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# Remove static files dirs that don't exist
+STATICFILES_DIRS = []
+
+# Disable migrations for faster tests
+class DisableMigrations:
+    def __contains__(self, item):
+        return True
+    
+    def __getitem__(self, item):
+        return None
+
+MIGRATION_MODULES = DisableMigrations()
+
+# Faster password hashing for tests
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
+
+# Disable logging during tests
+LOGGING_CONFIG = None
+
+# Test-specific settings
+DEBUG = False
+TEMPLATE_DEBUG = False
