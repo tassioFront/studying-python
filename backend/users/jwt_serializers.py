95bb822e9c47726dddf51ab448394645d7fe9deb
tassioFront <tassio72@hotmail.com>
2025-08-06 18:37:05 -0300

    feat: add custom JWT serializer and include user type in token claims

---
+++ backend/users/jwt_serializers.py
@@ -0,0 +1,10 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Add custom claims
+        token['type'] = user.type
+        token['is_active'] = user.is_active
+        return token
