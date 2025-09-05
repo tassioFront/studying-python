9b918c495482346a0b0cc8922ffda0bf6011cf3d
tassioFront <tassio72@hotmail.com>
2025-09-05 15:29:35 -0300

    debug: log SECRET_KEY for development purposes

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY", default="your-secret-key-for-development")
-
+print('SECRET_KEYYYY', SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True, cast=bool)
 
